c0064b694ee14967355679a312afd0bbc6b7adfb
ricks_ind <rishabh.hs@gmail.com>
2016-08-10 15:54:10 -0700

    added maps STL to replace roman charaters to long format

--- romanCalculator.cpp
+++ romanCalculator.cpp
@@ -10,9 +10,23 @@
 #include <string.h>
 #include <string>
 #include <ctype.h>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
 
 using namespace std;
 
+//creating maps to replace my lesser Roman numbers
+map <string, string> myRoman= {
+			{"IV", "IIII"},
+			{"IX", "VIIII"},
+			{"XL", "XXXX"},
+			{"XC", "LXXXX"},
+			{"CD", "CCCC"},
+			{"CM", "DCCCC"}	};
+
+//check for illegal characters entered by the user
 bool checkForIllegalCharacters(string &str){
 	for (unsigned int i = 0; i < str.length(); i++) {
 		if(!(str[i]=='M' ||str[i]=='D' ||str[i]=='C' ||str[i]=='L' ||str[i]=='X' ||str[i]=='V' ||str[i]=='I')){
@@ -22,20 +36,33 @@ bool checkForIllegalCharacters(string &str){
 	return true;
 }
 
+//convert the Roman number entered by user to Capital letters
 void convertToUpper(string &str) {
-	for (unsigned int i = 0; i < str.length(); i++) {
+	int len = str.length();
+	for (unsigned int i = 0; i <len; i++) {
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = toupper(str[i]);
 	}
 }
 
+bool IfReplacementRequired(char str1, char str2){
+	if(str1=='I' && str2=='V')
+		return false;
+	return true;
+}
+
+
+
 
 
 int main( int argc, char *argv[] ) {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Welcome to Roman Calculator!!!" << endl; // prints !!!Hello World!!!
+	vector <int> testing;
+
+
 	bool validate;
-	string roman_number1 = "mc";
-	string roman_number2 = "R";
+	string roman_number1 = "mclxiv";
+	string roman_number2 = "1V";
 
 
 	convertToUpper(roman_number1);
